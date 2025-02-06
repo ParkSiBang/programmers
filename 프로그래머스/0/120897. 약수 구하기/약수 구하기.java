67e831b3572f72a45db83d273dba9d6c3880f5f2
ParkSiBang <114298902+ParkSiBang@users.noreply.github.com>
2025-02-06 23:07:55 +0900

    [level 0] Title: 약수 구하기, Time: 2.91 ms, Memory: 91.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> divisors = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+            }
+        }
+        
+        return divisors.stream().mapToInt(i -> i).toArray();
+    }
+}
