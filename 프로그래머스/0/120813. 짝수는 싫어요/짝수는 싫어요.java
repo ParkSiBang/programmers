0d47f71d852ec7b7ae11f7fcb8f5269802457e0f
ParkSiBang <114298902+ParkSiBang@users.noreply.github.com>
2025-02-06 22:59:31 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.76 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= n; i += 2) {
+            result.add(i);
+        }
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
